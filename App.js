71ae1c5ded9b0657bd6d011bb9684ed5c65e654c
Hamish <hamisharro@icloud.com>
2021-08-05 16:18:05 +0100

    update app.js

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-import { StatusBar } from "expo-status-bar";
 import * as React from "react";
 import { View, TouchableWithoutFeedback, Text } from "react-native";
+
 import { GLView } from "expo-gl";
 import { Renderer } from "expo-three";
+import { TweenMax } from "gsap";
 
 import {
   AmbientLight,
@@ -17,67 +18,96 @@ import {
   SpotLight,
 } from "three";
 
-const sphere = new SphereMesh();
-const camera = new PerspectiveCamera(100, 0.4, 0.01, 1000);
+export default function App() {
+  const sphere = new SphereMesh();
+  const camera = new PerspectiveCamera(100, 0.4, 0.01, 1000);
 
-let cameraInitialPositionX = 0;
-let cameraInitialPositionY = 2;
-let cameraInitialPositionZ = 5;
+  let cameraInitialPositionX = 0;
+  let cameraInitialPositionY = 2;
+  let cameraInitialPositionZ = 5;
 
-export default class App extends Component {
-  render() {
-    return;
-    <GLView
-      style={{ flex: 1 }}
-      onContextCreate={async (gl) => {
-        // GL Parameter disruption
-        const { drawingBufferWidth: width, drawingBufferHeight: height } = gl;
+  function move(distance) {
+    TweenMax.to(sphere.position, 0.2, {
+      z: sphere.position.z + distance,
+    });
 
-        // Renderer declaration and set properties
-        const renderer = new Renderer({ gl });
-        renderer.setSize(width, height);
-        renderer.setClearColor("#fff");
-
-        // Scene declaration, add a fog, and a grid helper to see axes dimensions
-        const scene = new Scene();
-        scene.fog = new Fog("#3A96C4", 1, 10000);
-        scene.add(new GridHelper(10, 10));
-
-        // Add all necessary lights
-        const ambientLight = new AmbientLight(0x101010);
-        scene.add(ambientLight);
-
-        const pointLight = new PointLight(0xffffff, 2, 1000, 1);
-        pointLight.position.set(0, 200, 200);
-        scene.add(pointLight);
-
-        const spotLight = new SpotLight(0xffffff, 0.5);
-        spotLight.position.set(0, 500, 100);
-        spotLight.lookAt(scene.position);
-        scene.add(spotLight);
-
-        // Add sphere object instance to our scene
-        scene.add(sphere);
-
-        // Set camera position and look to sphere
-        camera.position.set(
-          cameraInitialPositionX,
-          cameraInitialPositionY,
-          cameraInitialPositionZ
-        );
-
-        camera.lookAt(sphere.position);
-
-        // Render function
-        const render = () => {
-          requestAnimationFrame(render);
-          renderer.render(scene, camera);
-          gl.endFrameEXP();
-        };
-        render();
-      }}
-    />;
+    TweenMax.to(camera.position, 0.2, {
+      z: camera.position.z + distance,
+    });
   }
+
+  return (
+    <View style={{ flex: 1 }}>
+      <GLView
+        style={{ flex: 1 }}
+        onContextCreate={async (gl) => {
+          // GL Parameter disruption
+          const { drawingBufferWidth: width, drawingBufferHeight: height } = gl;
+
+          // Renderer declaration and set properties
+          const renderer = new Renderer({ gl });
+          renderer.setSize(width, height);
+          renderer.setClearColor("#fff");
+
+          // Scene declaration, add a fog, and a grid helper to see axes dimensions
+          const scene = new Scene();
+          scene.fog = new Fog("#3A96C4", 1, 10000);
+          scene.add(new GridHelper(10, 10));
+
+          // Add all necessary lights
+          const ambientLight = new AmbientLight(0x101010);
+          scene.add(ambientLight);
+
+          // Add sphere object instance to our scene
+          scene.add(sphere);
+
+          // Set camera position and look to sphere
+          camera.position.set(
+            cameraInitialPositionX,
+            cameraInitialPositionY,
+            cameraInitialPositionZ
+          );
+
+          camera.lookAt(sphere.position);
+
+          // Render function
+          const render = () => {
+            requestAnimationFrame(render);
+            renderer.render(scene, camera);
+            gl.endFrameEXP();
+          };
+          render();
+        }}
+      >
+        <View>
+          <TouchableWithoutFeedback onPressIn={() => move(-0.2)}>
+            <Text
+              style={{
+                fontSize: 36,
+                MozUserSelect: "none",
+                WebkitUserSelect: "none",
+                msUserSelect: "none",
+              }}
+            >
+              UP
+            </Text>
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback onPressIn={() => move(0.2)}>
+            <Text
+              style={{
+                fontSize: 36,
+                MozUserSelect: "none",
+                WebkitUserSelect: "none",
+                msUserSelect: "none",
+              }}
+            >
+              DOWN
+            </Text>
+          </TouchableWithoutFeedback>
+        </View>
+      </GLView>
+    </View>
+  );
 }
 
 class SphereMesh extends Mesh {
@@ -90,12 +120,3 @@ class SphereMesh extends Mesh {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
